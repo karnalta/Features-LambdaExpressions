3929a5c2b32be56021762176ff43aee768554e61
Dimitri Mathieu <dimitri.mathieu@outlook.com>
2019-08-09 13:19:19 +0200

    Ajoutez des fichiers projet.

---
+++ Features-LambdaExpressions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Features_LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Local action
+            Action<string, int> localAction = (name, value) => { Console.WriteLine($"Executing {name} with value {value}."); };
+            localAction("local action", 1);
+
+            Console.WriteLine();
+
+            // Action from a method
+            var methodAction = CreateAction(2);
+            methodAction(3);
+
+            Console.WriteLine();
+
+            // Func from a method
+            Func<string, int, int> localFunc = (name, value) => 
+            {
+                Console.WriteLine($"Executing {name} with value {value}.");
+
+                return 6;
+            };
+            Console.WriteLine($"And returned {localFunc("local func", 5)}");
+
+            Console.WriteLine();
+
+            // Func to a method
+            ExecuteFunc((value) => { return value * 10; });
+
+            Console.WriteLine();
+
+            // Local expression
+            Expression<Func<int, int, int>> localExp = (x, y) => x * y;
+            Console.WriteLine($"Local expression to string => { localExp }");
+
+            // Compiling and running the local expression
+            Console.WriteLine($"Local expression result after compilation => { localExp.Compile()(5, 4) }");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Creates an action.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        static Action<int> CreateAction(int value)
+        {
+            Action<int> action = (localValue) => { Console.WriteLine($"Executing method action with method value {value} and local value {localValue}."); };
+
+            return action;
+        }
+
+        /// <summary>
+        /// Executes a function.
+        /// </summary>
+        /// <param name="func">The function.</param>
+        static void ExecuteFunc(Func<int, int> func)
+        {
+            for (int i = 1; i <= 3; i++)
+                Console.WriteLine($"Executing method func with iteration {i} and it returned { func(i) }.");
+        }
+    }
+}
---
+++ Features-LambdaExpressions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Features-LambdaExpressions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Features-LambdaExpressions")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("67b2de3e-ea22-4f7a-a2b0-0337fc2ef900")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
